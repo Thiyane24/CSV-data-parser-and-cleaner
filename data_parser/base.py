6bcd913b589ce2a8b3225e15ac5cd398ddcc2bd3
Thiyane Xavier <thiyanexavier6@gmail.com>
2026-03-09 22:52:27 +0800

    DataFile Class complete and tested

--- data_parser/base.py
+++ data_parser/base.py
@@ -1,13 +1,29 @@
 class DataFile:
     def __init__(self, filepath,  data = None, is_loaded = False):
+        """Initiates the class DataFile
+        """
         self.filepath = filepath 
         self.data = data
         self.is_loaded = is_loaded 
         
     def __str__(self):
-        pass
+        """ shows the number of rows in the data we key in """
+        rows = len(self.data) if self.data else "not loaded"
+        return f"File: {self.filepath} | Rows: {rows}"
     
     def __repr__(self):
-        pass
+        """Returns the information about the DataFile
+        """
+        return f"DataFile(filepath = '{self.filepath}', loaded= {self.is_loaded})"
     
+    def read(self):
+        """Raises a NotImplementedError
+        """
+        raise NotImplementedError("Subclasses must implement read")
+    
+test= DataFile("top100_ai_tools_2026.csv")
+
+print(test)
+
+
     
